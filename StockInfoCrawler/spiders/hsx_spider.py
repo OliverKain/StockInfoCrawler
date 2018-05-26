802d2d3cd602158f276733c74b43f582909f33ee
Americio <hoangquocmy@gmail.com>
2018-05-26 15:51:46 +0700

    Fixed hsx -spider

--- StockInfoCrawler/spiders/hsx_spider.py
+++ StockInfoCrawler/spiders/hsx_spider.py
@@ -17,17 +17,18 @@ class HsxSpider(scrapy.Spider):
     custom_settings = {
         "FEED_FORMAT": "csv",
         "FEED_URI": "data/hsx.csv",
+        "DNS_TIMEOUT": "10",
     }
 
     def __init__(self, **kwargs):
         init_url = "https://www.hsx.vn/Modules/CMS/Web/ArticleInCategory/dca0933e-a578-4eaf-8b29-beb4575052c5"\
-                   + "?exclude=00000000-0000-0000-0000-000000000000&lim=True&pageFieldName1=FromDate"\
-                   + "&pageFieldValue1={0}&pageFieldOperator1=eq&pageFieldName2=ToDate"\
-                   + "&pageFieldValue2={1}&pageFieldOperator2=eq&pageFieldName3=TokenCode"\
-                   + "&pageFieldValue3=&pageFieldOperator3=eq&pageFieldName4=CategoryId"\
-                   + "&pageFieldValue4=dca0933e-a578-4eaf-8b29-beb4575052c5"\
-                   + "&pageFieldOperator4=eq&pageCriteriaLength=4"\
-                   + "&_search=false&nd=1526850145275&rows=30&page={2}&sidx=id&sord=desc"
+                   "?exclude=00000000-0000-0000-0000-000000000000&lim=True" \
+                   "&pageFieldName1=FromDate&pageFieldValue1={0}&pageFieldOperator1=eq" \
+                   "&pageFieldName2=ToDate&pageFieldValue2={1}&pageFieldOperator2=eq" \
+                   "&pageFieldName3=TokenCode&pageFieldValue3=&pageFieldOperator3=eq" \
+                   "&pageFieldName4=CategoryId&pageFieldValue4=dca0933e-a578-4eaf-8b29-beb4575052c5"\
+                   "&pageFieldOperator4=eq&pageCriteriaLength=4"\
+                   "&_search=false&nd=1526850145275&rows=30&page={2}&sidx=id&sord=desc"
         list_json_link = init_url.format(self.last_two_weeks_str, self.today, 1)
         response = requests.get(url=list_json_link)
         response_json = json.loads(response.text)
