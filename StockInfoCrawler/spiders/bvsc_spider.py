1ca1443e7cd604ce29b2e4be9064eb95e7f04b60
My Hoang <hoangquocmy@gmail.com>
2018-11-14 17:09:13 +0700

    Maintenance bvsc-spider
    Added summary field in result

--- StockInfoCrawler/spiders/bvsc_spider.py
+++ StockInfoCrawler/spiders/bvsc_spider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy.http import HtmlResponse
+from scrapy.http import Request
 from datetime import date, timedelta
 from commons.is_in_filtered_time import is_in_filtered_time
 
@@ -30,13 +31,13 @@ class BvscSpider(scrapy.Spider):
             for s in range(1, self.max_depth + 1):
                 self.start_urls.append(
                     ("http://www.bvsc.com.vn/ViewReports.aspx?CategoryID=17&StartDate={0}&EndDate={1}"
-                    + "&Cart_ctl00_webPartManager_wp484641337_wp148882691_cbReports_Callback_Param={2}")
+                     + "&Cart_ctl00_webPartManager_wp484641337_wp148882691_cbReports_Callback_Param={2}")
                     .format(self.last_week_str, self.today, (s - 1) * 20))
         else:
             for s in range(1, self.max_depth + 1):
                 self.start_urls.append(
                     ("http://www.bvsc.com.vn/ViewReports.aspx?CategoryID=33&StartDate={0}&EndDate={1}"
-                    + "&Cart_ctl00_webPartManager_wp484641337_wp148882691_cbReports_Callback_Param={2}")
+                     + "&Cart_ctl00_webPartManager_wp484641337_wp148882691_cbReports_Callback_Param={2}")
                     .format(self.last_week_str, self.today, (s - 1) * 20))
         super().__init__(**kwargs)
 
@@ -49,10 +50,24 @@ class BvscSpider(scrapy.Spider):
         for article in article_list:
             time_str = get_time(article.xpath(self.time_xpath).extract_first().strip())
             if is_in_filtered_time(time_str):
+                article_link = article.xpath(self.link_xpath).extract_first().strip()
                 article_detail = {"title": article.xpath(self.title_xpath).extract_first().strip(),
                                   "time": time_str,
-                                  "link": article.xpath(self.link_xpath).extract_first().strip()}
-                yield article_detail
+                                  "init": "",
+                                  "link": article_link}
+                yield Request(url=article_link, callback=self.get_summary, meta={"article_detail": article_detail})
+
+    @staticmethod
+    def get_summary(response):
+        # Get article detail from request
+        article_detail = response.meta.get("article_detail")
+        # Get summary
+        summary_xpath = response.selector.xpath("//td[@class='report_row_last']")[4]
+        for paragraph_xpath in summary_xpath.xpath("./p"):
+            # Append paragraphs
+            paragraph = "".join(paragraph_xpath.xpath(".//text()").extract()).strip()
+            article_detail["init"] += paragraph + "\r\n"
+        yield article_detail
 
 
 def get_time(time_str):
