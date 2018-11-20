5965bdd731b9c9a3b7c3953911af27ff3a3c5d72
My Hoang <hoangquocmy@gmail.com>
2018-11-20 13:08:56 +0700

    Maintenance hnx-spider
    Code Convention

--- StockInfoCrawler/spiders/hnx_spider.py
+++ StockInfoCrawler/spiders/hnx_spider.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.http import Request, FormRequest
+from scrapy.http import FormRequest
 from commons.is_in_filtered_time import is_in_filtered_time
 
 
@@ -31,26 +31,24 @@ class HnxSpider(scrapy.Spider):
     def start_requests(self):
         for url in self.start_urls:
             yield FormRequest(url, callback=self.parse, formdata=None, method="POST",
-                        cookies={
-                            "ASP.NET_SessionId": "o4paaccs0rfnrsfwxkp04ja1",
-                            "language": "vi-VN"
-                        })
+                              cookies={
+                                  "ASP.NET_SessionId": "o4paaccs0rfnrsfwxkp04ja1",
+                                  "language": "vi-VN"
+                              })
 
     def parse(self, response):
         article_list = response.selector.xpath(self.list_xpath)
         for article in article_list:
-            title = ""
-            init = ""
             if response.request.url.find("NextPageTinCPNY_CBTCPH") != -1:
                 title = article.xpath(self.stock_id_xpath).extract_first().strip() \
-                            + " - " + article.xpath(self.org_name_xpath).extract_first().strip()
+                        + " - " + article.xpath(self.org_name_xpath).extract_first().strip()
                 init = article.xpath(self.title_issuer_xpath).extract_first().strip()
             else:
                 title = article.xpath(self.stock_id_xpath).extract_first().strip()
                 init = article.xpath(self.title_hnx_xpath).extract_first().strip()
             article_detail = {"title": title,
                               "time": get_time(article.xpath(self.time_xpath).extract_first().strip()),
-                              "init": init,}
+                              "init": init}
             if is_in_filtered_time(article_detail.get("time")):
                 yield article_detail
 
